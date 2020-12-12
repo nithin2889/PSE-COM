23c437079720b76ef74926f894f0927cb8a83536
akhilamartia <akhila.martia@gmail.com>
2020-12-12 22:58:25 +0530

    added pagination, carousel and search functionality

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keyword" content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Welcome To LPPL",
+  description: "Any Property Any Place",
+  keywords: "real estate, litigation properties, litigation, properties",
+};
+
+export default Meta;
